b35b2257fb1ef8131de6ad5e45e7c126138c6a7d
Paul Weerheim <paul@maris.nl>
2021-02-09 19:00:40 +0100

    Removed sub-indicators to make stuff simpler

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 const fs = require("fs");
 const fse = require("fs-extra");
 const config = require("./config");
-const ejs = require('ejs')
-const rimraf = require('rimraf');
+const ejs = require("ejs");
+const rimraf = require("rimraf");
 
-const APP_URL = "https://cds.climate.copernicus.eu/workflows/c3s/%APP%/master/configuration.json?configuration_version=3.0";
+const APP_URL =
+  "https://cds.climate.copernicus.eu/workflows/c3s/%APP%/master/configuration.json?configuration_version=3.0";
 
 // create output dir or empty output dir
 if (!fs.existsSync(config.dev.outdir)) {
-    fs.mkdirSync(config.dev.outdir);
-
+  fs.mkdirSync(config.dev.outdir);
 } else {
-    rimraf.sync(config.dev.outdir);
+  rimraf.sync(config.dev.outdir);
 
-    if (!fs.existsSync(config.dev.outdir)) {
-        fs.mkdirSync(config.dev.outdir);
-    }
+  if (!fs.existsSync(config.dev.outdir)) {
+    fs.mkdirSync(config.dev.outdir);
+  }
 }
 
-const srcPath = './src'
+const srcPath = "./src";
 const outputDir = config.dev.outdir;
 
 const data_apps = JSON.parse(fs.readFileSync("./src/data_apps.json", "utf-8"));
-const data_themes = JSON.parse(fs.readFileSync("./src/data_themes.json", "utf-8"));
-const data_overview = JSON.parse(fs.readFileSync("./src/data_overview.json", "utf-8"));
+const data_themes = JSON.parse(
+  fs.readFileSync("./src/data_themes.json", "utf-8")
+);
+const data_overview = JSON.parse(
+  fs.readFileSync("./src/data_overview.json", "utf-8")
+);
 
-Object.assign(data_themes, data_apps); 
+Object.assign(data_themes, data_apps);
 
 // copy assets to output dir
 fse.copy(`${srcPath}/assets`, outputDir);
@@ -38,170 +42,160 @@ createOverviewPages(data_overview);
 
 createIndexPage();
 
-function createIndexPage(){
-    ejs.renderFile(`${srcPath}/templates/index.ejs`, {}, (err, data) => {
-        if (err) throw (err);
-        fs.writeFile(`${outputDir}/index.html`, data, (err) => {
-            if (err) throw (err);
-            console.log(`index.html has been created.`);
-        })
+function createIndexPage() {
+  ejs.renderFile(`${srcPath}/templates/index.ejs`, {}, (err, data) => {
+    if (err) throw err;
+    fs.writeFile(`${outputDir}/index.html`, data, (err) => {
+      if (err) throw err;
+      console.log(`index.html has been created.`);
     });
+  });
 }
 
-function createOverviewPages(overview_data){
-    ejs.renderFile(`${srcPath}/templates/overview-list.ejs`, overview_data, (err, data) => {
-        if (err) throw (err);
-        fs.writeFile(`${outputDir}/overview-list.html`, data, (err) => {
-            if (err) throw (err);
-            console.log(`overview-list.html has been created.`);
-        })
-    })
+function createOverviewPages(overview_data) {
+  ejs.renderFile(
+    `${srcPath}/templates/overview-list.ejs`,
+    overview_data,
+    (err, data) => {
+      if (err) throw err;
+      fs.writeFile(`${outputDir}/overview-list.html`, data, (err) => {
+        if (err) throw err;
+        console.log(`overview-list.html has been created.`);
+      });
+    }
+  );
 }
 
 function createAppPages(data) {
-    for (const index in data["datasets"]) {
-        const dataset = data["datasets"][index];
-        dataset.overview = APP_URL.replace("%APP%", dataset.overview);
-        dataset.detail = APP_URL.replace("%APP%", dataset.detail);
+  for (const index in data["datasets"]) {
+    const dataset = data["datasets"][index];
+    dataset.overview = APP_URL.replace("%APP%", dataset.overview);
+    dataset.detail = APP_URL.replace("%APP%", dataset.detail);
 
-        // theme directory
-        if (!fs.existsSync(`${outputDir}/${(dataset.theme).toLowerCase()}/`)) {
-            fs.mkdirSync(`${outputDir}/${(dataset.theme).toLowerCase()}/`)
-        }
-
-        if (dataset.indicators !== undefined) {
-            dataset.indicators.forEach(indicator => {
-                createHTMLfiles(dataset, indicator)
-            });
-        } else {
-            createHTMLfiles(dataset)
-        }
+    // theme directory
+    if (!fs.existsSync(`${outputDir}/${dataset.theme.toLowerCase()}/`)) {
+      fs.mkdirSync(`${outputDir}/${dataset.theme.toLowerCase()}/`);
     }
+
+    createHTMLfiles(dataset);
+  }
 }
 
 function overviewFileName(dataset, indicator = null) {
-    let name = null
+  let name = null;
 
-    // URL override
-    if (dataset.page_url) {
-        name = dataset.page_url
-    } else {
-        name = slugify(dataset.indicator_title)
-    }
+  // URL override
+  if (dataset.page_url) {
+    name = dataset.page_url;
+  } else {
+    name = slugify(dataset.indicator_title);
+  }
 
-    if (indicator) {
-        name += `--${slugify(indicator)}`
-    }
-    name += ".html"
-    return name
+  if (indicator) {
+    name += `--${slugify(indicator)}`;
+  }
+  name += ".html";
+  return name;
 }
 
 function detailFileName(dataset, indicator = null) {
-    let name = null
+  let name = null;
 
-    // URL override
-    if (dataset.page_url) {
-        name = dataset.page_url
-    } else {
-        name = slugify(dataset.indicator_title)
-    }
+  // URL override
+  if (dataset.page_url) {
+    name = dataset.page_url;
+  } else {
+    name = slugify(dataset.indicator_title);
+  }
 
-    if (indicator) {
-        name += `--${slugify(indicator)}`
-    }
-    name += "-detail.html"
-    return name
+  if (indicator) {
+    name += `--${slugify(indicator)}`;
+  }
+  name += "-detail.html";
+  return name;
 }
 
 function slugify(string) {
-    return string.toLowerCase().replace(/\s/g, "-").replace(/_/g, "-")
+  return string.toLowerCase().replace(/\s/g, "-").replace(/_/g, "-");
 }
 
-function createHTMLfiles(dataset, indicator = null) {
-    if (!dataset.overview_var){
-        dataset.overview_var = null
-    }
-    
-    if (!dataset.detail_var){
-        dataset.detail_var = null;
-    }
+function createHTMLfiles(dataset) {
+  //create overview and detail pages for each app
+  if (!dataset.overview_var) {
+    dataset.overview_var = null;
+  }
 
-    if (indicator) {
+  if (!dataset.detail_var) {
+    dataset.detail_var = null;
+  }
 
-        dataset.overviewpage = overviewFileName(dataset, indicator.name);
-        dataset.detailpage = detailFileName(dataset, indicator.name);
-        dataset.description_detail = indicator.description_detail
-        dataset.page_title = indicator.page_title
-        dataset.indicator_title = indicator.indicator_title
-        dataset.description = indicator.description
-        dataset.overview_var = indicator.overview_var
-        dataset.detail_var = indicator.detail_var
+  dataset.overviewpage = overviewFileName(dataset);
+  dataset.detailpage = detailFileName(dataset);
+  dataset.indicator_title = dataset.indicator_title;
 
-    } else {
-        
-        dataset.overviewpage = overviewFileName(dataset);
-        dataset.detailpage = detailFileName(dataset);
-        dataset.indicator_title = dataset.indicator_title
-        
-    }
+  // overview page
+  let overviewFile = `${srcPath}/templates/overview.ejs`;
 
-    // overview page
-    let overviewFile = `${srcPath}/templates/overview.ejs`
+  ejs.renderFile(overviewFile, dataset, (err, data) => {
+    if (err) throw err;
+    fs.writeFile(
+      `${outputDir}/${dataset.theme.toLowerCase()}/${dataset.overviewpage}`,
+      data,
+      (err) => {
+        if (err) throw err;
+        console.log(`${dataset.overviewpage} has been created.`);
+      }
+    );
+  });
 
-    ejs.renderFile(overviewFile, dataset, (err, data) => {
-        if (err) throw (err);
-        fs.writeFile(`${outputDir}/${(dataset.theme).toLowerCase()}/${dataset.overviewpage}`, data, (err) => {
-            if (err) throw (err);
-            console.log(`${dataset.overviewpage} has been created.`);
-        })
-    })
-
-    // detail page
-    ejs.renderFile(`${srcPath}/templates/detail.ejs`, dataset, (err, data) => {
-        if (err) throw (err);
-        fs.writeFile(`${outputDir}/${(dataset.theme).toLowerCase()}/${dataset.detailpage}`, data, (err) => {
-            if (err) throw (err);
-            console.log(`${dataset.detailpage} has been created.`);
-        })
-    })
+  // detail page
+  ejs.renderFile(`${srcPath}/templates/detail.ejs`, dataset, (err, data) => {
+    if (err) throw err;
+    fs.writeFile(
+      `${outputDir}/${dataset.theme.toLowerCase()}/${dataset.detailpage}`,
+      data,
+      (err) => {
+        if (err) throw err;
+        console.log(`${dataset.detailpage} has been created.`);
+      }
+    );
+  });
 }
-
 
 function createThemePages(data) {
-    for (const index in data["themes"]) {
-        const theme = data["themes"][index]
-        theme.apps = []
+  //voor elk thema maken we een pagina aan
+  for (const index in data["themes"]) {
+    const theme = data["themes"][index];
+    theme.apps = [];
 
-        for (const indexApp in data["datasets"]) {
-            const dataset = data["datasets"][indexApp]
-            if (theme.theme_title.toLowerCase() == dataset.theme.toLowerCase() && !dataset.exclude) {
-                if (dataset.indicators !== undefined) {
-                    dataset.indicators.forEach(indicator => {
-                        theme.apps.push({
-                            "title": indicator.indicator_title,
-                            "url": `${(theme.theme_title).toLowerCase()}/${overviewFileName(dataset, indicator.name)}`
-                        })
+    //verzamel actieve apps en voeg titel+links toe aan theme.apps[]
+    for (const app_index in data["datasets"]) {
+      const dataset = data["datasets"][app_index];
 
-                    });
-                } else {
-                    theme.apps.push({
-                        "title": dataset.page_title,
-                        "url": `${(theme.theme_title).toLowerCase()}/${overviewFileName(dataset)}`
-                    })
-                }
-            }
-        }
-
-        // sort apps by title
-        theme.apps.sort((a, b) => a.title.localeCompare(b.title));
-
-        ejs.renderFile(`${srcPath}/templates/theme.ejs`, theme, (err, data) => {
-            if (err) throw (err);
-            const outputFile = `${(theme.theme_title).toLowerCase()}.html`
-            fs.writeFile(`${outputDir}/${outputFile}`, data, (err) => {
-                if (err) throw (err);
-                console.log(`${outputFile} has been created.`);
-            })
-        })
+      if (
+        theme.theme_title.toLowerCase() == dataset.theme.toLowerCase() &&
+        !dataset.exclude
+      ) {
+        theme.apps.push({
+          title: dataset.page_title,
+          url: `${theme.theme_title.toLowerCase()}/${overviewFileName(
+            dataset
+          )}`,
+        });
+      }
     }
+
+    // sort apps by title
+    theme.apps.sort((a, b) => a.title.localeCompare(b.title));
+
+    //render html
+    ejs.renderFile(`${srcPath}/templates/theme.ejs`, theme, (err, data) => {
+      if (err) throw err;
+      const outputFile = `${theme.theme_title.toLowerCase()}.html`;
+      fs.writeFile(`${outputDir}/${outputFile}`, data, (err) => {
+        if (err) throw err;
+        console.log(`${outputFile} has been created.`);
+      });
+    });
+  }
 }
