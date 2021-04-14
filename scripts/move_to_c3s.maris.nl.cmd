8c14eee4729c774b43a903096c7c6c40221a1972
Paul Weerheim <paul@maris.nl>
2021-04-14 09:13:25 +0200

    Portal updates

--- scripts/move_to_c3s.maris.nl.cmd
+++ scripts/move_to_c3s.maris.nl.cmd
@@ -7,12 +7,27 @@ IF /I NOT "%cd%" == "P:\paul\Workspaces\c3s" (
     ECHO Not in correct directory.
     EXIT
 ) ELSE (
-    ECHO Copying files from public\ to S:\www\c3s.maris.nl\
+    ECHO Removing files from S:\www\c3s.maris.nl\
 )
 
 
 @REM @ECHO on
+@REM Remove directories
+ FOR /D %%d IN (S:\www\c3s.maris.nl\*) DO (
+    @REM  echo "%%d"
+    rmdir /Q /S "%%d"
+ )
 
-for %%i in (*.*) do if not "%%i"==".gitignore" if not "%%i"==".git" del /q /s "%%i"
+@REM Remove files
+FOR %%i IN (S:\www\c3s.maris.nl\*) DO (
+    if not "%%i"=="S:\www\c3s.maris.nl\.gitignore" (
+        if not "%%i"=="S:\www\c3s.maris.nl\.git" (
+            @REM echo "%%i"
+            del /q /s "%%i"
+        )
+    )
+)
 
-COPY public\* S:\www\c3s.maris.nl /Y /V 
+ECHO Copying files from public\ to S:\www\c3s.maris.nl\
+
+XCOPY public\* S:\www\c3s.maris.nl /S /Y /V 
